6a4360e8651e26c20b1e1c1b3f2cbab0b17f2b7c
Jevenson  Peñas <jevenson.p.7789@gmail.com>
2024-09-13 14:46:11 +0800

    Creating Model and Migrations

---
+++ database/migrations/2024_09_13_055948_create_positions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->string('position_name'); 
+            $table->string('reports_to')->nullable(); 
+            $table->timestamps(); 
+
+            // Index position_name for faster searching . 
+            $table->index('position_name'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('positions');
+    }
+};
